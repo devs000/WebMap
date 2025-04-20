0a6bcd9f01f1bf200c86c251ee23594e3c163502
devs000 <abdevs@tuta.io>
2025-04-20 20:12:14 +0200

    Update urls.py

--- docker/urls.py
+++ docker/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
 
+# Définition des routes (URLs) principales de l'application
 urlpatterns = [
-	path('', include('nmapreport.urls')),
-	path('report/', include('nmapreport.urls')),
-	path('admin/', admin.site.urls),
+    # Route principale : Redirige toutes les requêtes vers l'application `nmapreport`
+    path('', include('nmapreport.urls')),
+    # ^ Cette route gère la page d'accueil et redirige vers les URLs définies dans `nmapreport.urls`.
+	
+    # Route spécifique pour les rapports : Redirige également vers `nmapreport.urls`
+    path('report/', include('nmapreport.urls')),
+    # ^ Cette route permet d'accéder aux fonctionnalités de rapport via `/report/`.
+
+    # Route pour l'interface d'administration Django
+    path('admin/', admin.site.urls),
+    # ^ Cette route active l'interface d'administration Django, accessible via `/admin/`.
 ]
